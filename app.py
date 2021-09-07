d246a2d6074390b2235d4f0878025f42e95542de
benber86 <benoitb@tutanota.de>
2021-09-07 22:38:46 +1000

    better caching

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 import os
-import requests_cache
 from dotenv import load_dotenv
+from flask_caching import Cache
 
 load_dotenv()
 
 from modules.gas_price_locked_cvx import get_locked_crv_and_gas_price
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 @app.route("/data/gaspricelocked")
+@cache.cached(timeout=60*60)
 def pricedatadtd():
     return get_locked_crv_and_gas_price()
 
@@ -24,7 +26,7 @@ def gas_locked():
 def home():
     return app.send_static_file('index.html')
 
+
 if __name__ == '__main__':
-    requests_cache.install_cache(cache_name='github_cache', backend='sqlite', expire_after=60*60)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
